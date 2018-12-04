72832a11c180aeb7d7821625fe44068bd1915cd6
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-05 08:39:35 +0900

    [Update] アルファ版の更新

--- alpha/static/js/screen-transition.js
+++ alpha/static/js/screen-transition.js
@@ -1,8 +1,8 @@
 // ローディング中の画面チラツキ防止
-$(window).on('load', function() {
-    $('body')
-        .css('visibility','initial')
-        .css('overflow', 'initial')
+$(window).on("load", function() {
+    $("body")
+        .css("visibility","initial")
+        .css("overflow", "initial")
         .hide()
         .fadeIn("slow");
 });
